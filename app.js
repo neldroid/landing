52fa665c5759500e1c41665bc87747c99bbc010e
Nahuel <nahuelul@gmail.com>
2024-05-01 23:22:51 +0200

    Bottom section, remove material

---
+++ app.js
@@ -0,0 +1,7 @@
+function registerEmail(){
+    console.log(document.getElementById("emailInput").value)
+}
+
+function scrollToSection(section){
+    document.getElementById(section).scrollIntoView();
+}
